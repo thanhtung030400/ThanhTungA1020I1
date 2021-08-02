76c8c3cf072d669b5eb4967ae449fc76e46fd17d
thanhtung030400 <thanhtung030400@gmail.com>
2021-08-03 00:41:42 +0700

    update

---
+++ Module_4/bai_14_Spring_security/thuc_hanh/src/main/java/com/codegym/demo/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.codegym.demo;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
